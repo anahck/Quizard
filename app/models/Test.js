6aad70d81f786076769558507793b503061b52b6
tfs18 <tfitzsmith1@gmail.com>
2025-08-26 10:54:26 +0100

    add show test route

--- app/models/Test.js
+++ app/models/Test.js
@@ -17,6 +17,27 @@ class Test {
         }
         return response.rows.map(test => new Test(test))
     }
+
+    static async getOneByID(test) {
+        const response = await db.query("SELECT * FROM test WHERE testid = $1;", [test])
+        if (response.rows.length !== 1) {
+            throw Error("Unable to locate test")
+        }
+        return new Test(response.rows[0])
+    }
+
+    static async create(data){
+        const {testname, subjectid, duedate, assigneddate, authorid} = data
+        const existingUser = await db.query("SELECT email FROM userinfo WHERE email = $1", [email])
+        if(existingUser.rows.length === 0){
+            const response = await db.query("INSERT INTO userinfo (firstname, lastname, email, passwordhash, userrole, yeargroup) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+                [firstname, lastname, email, passwordhash, userrole, yeargroup]);
+            return new UserInfo(response.rows[0])
+        }
+        else{
+            throw new Error("A user with this email already exists")
+        }
+    }
 }
 
 module.exports = Test
