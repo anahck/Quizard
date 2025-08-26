ad8f4654277e67493de875224793aa65d29872e3
tfs18 <tfitzsmith1@gmail.com>
2025-08-26 10:24:09 +0100

    add update/destroy restful routes to UserInfo class

--- app/routes/userInfo.js
+++ app/routes/userInfo.js
@@ -8,5 +8,7 @@ userRouter.get('/', userController.index)
 // userRouter.get('/:email', userController.showEmail)
 userRouter.get('/:id', userController.showId)
 userRouter.post("/", userController.create)
+userRouter.patch("/:id", userController.update)
+userRouter.delete("/:id", userController.destroy)
 
 module.exports = userRouter
